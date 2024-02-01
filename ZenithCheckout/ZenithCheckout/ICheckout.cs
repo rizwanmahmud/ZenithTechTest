cb74d7fc9f5b1f2eeebe6c9b1615393f3eeaf18d
rizwanmahmud <rizwanmahmud@hotmail.co.uk>
2024-02-01 19:38:26 +0000

    Create ICheckout.cs
    Adding an interface for Checkout

---
+++ ZenithCheckout/ZenithCheckout/ICheckout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZenithCheckout
+{
+    interface ICheckout
+    {
+        void Scan(string item);
+        int GetTotalPrice();
+    }
+
+}
