2f48af7e6f72215f694d14311bc9a6ed50a093f6
rizwanmahmud <rizwanmahmud@hotmail.co.uk>
2024-02-01 19:57:49 +0000

    Update Checkout.cs
    Adding properties to the Checkout class to define:
    ScannedItems
    ItemPrices
    ItemMultiPrices

--- ZenithCheckout/ZenithCheckout/Checkout.cs
+++ ZenithCheckout/ZenithCheckout/Checkout.cs
@@ -8,6 +8,10 @@ namespace ZenithCheckout
 {
     public class Checkout : ICheckout
     {
+        private readonly List<char> scannedItems;
+        private readonly Dictionary<char, int> itemPrices;
+        private readonly Dictionary<char, Tuple<int, int>> itemMultiprices;
+
         public int GetTotalPrice()
         {
             throw new NotImplementedException();
