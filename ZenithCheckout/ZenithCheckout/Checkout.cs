3670736656c682241e2db990c8c7b3dcc35dded5
rizwanmahmud <rizwanmahmud@hotmail.co.uk>
2024-02-01 19:41:08 +0000

    Create Checkout.cs
    Adding a new class for Checkout which implements the ICheckout interfact

---
+++ ZenithCheckout/ZenithCheckout/Checkout.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZenithCheckout
+{
+    public class Checkout : ICheckout
+    {
+        public int GetTotalPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(string item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
